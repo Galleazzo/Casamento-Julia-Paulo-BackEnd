49acfa73223b7caef1f47d887cd03d9f48550df2
Galleazzo <paulogalleazzo1@gmail.com>
2026-06-19 00:55:24 +0000

    Empacotar Spring Boot como pacote de deploy AWS Lambda

---
+++ src/main/java/com/br/Julia/Paulo/StreamLambdaHandler.java
@@ -0,0 +1,31 @@
+package com.br.Julia.Paulo;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+    private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Não foi possível inicializar a aplicação Spring Boot", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
